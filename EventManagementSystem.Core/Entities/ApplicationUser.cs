50bcbfb849e0e0c7a1b56f77b82cb319d432c665
Abderrahman Abaakil <abderrahmanebkl48@gmail.com>
2025-08-23 18:53:12 +0200

    add the property created at for the user

--- EventManagementSystem.Core/Entities/ApplicationUser.cs
+++ EventManagementSystem.Core/Entities/ApplicationUser.cs
@@ -7,5 +7,6 @@ namespace EventManagementSystem.Infrastructure.Entities
     {
         public string FullName { get; set; } = string.Empty;
         public ICollection<Reservation> Reservations { get; set; } = new List<Reservation>();
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
